e8b4d8cd3908bf0adf5ec02230cfc720ced25a50
JuliZpta <zapatajulian42@gmail.com>
2023-09-19 11:15:41 -0500

    End

---
+++ Clase19Sept/app.js
@@ -0,0 +1,56 @@
+const usersChoice = document.createElement('h1');
+const computersChoice = document.createElement('h1');
+const resultDisplay = document.createElement('h1');
+
+//Encontrando el section del HTMl que tiene id = "root"
+const gameGrid = document.getElementById('root')
+gameGrid.append(usersChoice);
+gameGrid.append(computersChoice);
+gameGrid.append(resultDisplay);
+
+const choices=['Piedra','Papel','Tijera'];
+let userChoice;
+let computerChoice;
+
+const generateComputerPlay = () =>{
+    const randomChoice = choices[Math.floor(Math.random()*choices.length)];
+    computerChoice = randomChoice;
+    computersChoice.innerHTML = `La jugada de la máquina es: ${computerChoice}`; 
+}
+
+const generateResult = () =>{
+    switch (userChoice+computerChoice) {
+        case 'TijeraPapel':
+        case 'PiedraTijera':
+        case 'PapelPiedra':
+                resultDisplay.innerHTML = 'Ganó el usuario';
+                break;
+
+        case 'PiedraPapel':
+        case 'TijeraPiedra': 
+        case 'PapelTijera':
+                 resultDisplay.innerHTML = 'Perdió el usuario';
+                 break;
+        
+        default:
+            resultDisplay.innerHTML = 'Es un empate';
+            break;
+    }
+} 
+
+const handleClick = e =>{
+    userChoice = e.target.id;
+    usersChoice.innerHTML = 'Su jugada es: '+userChoice; 
+    generateComputerPlay();
+    generateResult();
+
+}
+
+for (let index = 0; index < choices.length; index++) {
+    const button = document.createElement('button');
+    button.id = choices[index];
+    button.innerHTML = choices[index];
+    //Cuando le haga click, llama la funcion HandleClick
+    button.addEventListener('click',handleClick); 
+    gameGrid.appendChild(button);                    
+}
