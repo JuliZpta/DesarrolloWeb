87c0fdb83617099ed158ea7d879ea4584777387a
JuliZpta <zapatajulian42@gmail.com>
2023-10-02 20:14:28 -0500

    Cambios js y css

--- EntregableDOM/script.js
+++ EntregableDOM/script.js
@@ -21,27 +21,43 @@ const ul = createAndAppend('ul', nav);
 
 createAndAppend('li', ul, createAndAppend('a', ul, 'Inicio'));
 createAndAppend('li', ul, createAndAppend('a', ul, 'Menú'));
-createAndAppend('li', ul, createAndAppend('a', ul, 'Promociones'));
+createAndAppend('li', ul, createAndAppend('a', ul, 'Promociones')).setAttribute('href', '#promociones'); // Agregamos el atributo href
 createAndAppend('li', ul, createAndAppend('a', ul, 'Contacto'));
+
 
 // Crear contenido principal
 const main = createAndAppend('main', body);
 
 const menuSection = createAndAppend('section', main);
-createAndAppend('h2', menuSection, 'Nuestro Menú');
+menuSection.classList.add('menu');
+
+const menuHeader = createAndAppend('h2', menuSection, 'Nuestro Menú');
+menuHeader.classList.add('menu-header');
 
 // Crear elementos del menú (ejemplo)
-const menuItem1 = createAndAppend('div', menuSection);
+const menuItem1 = createAndAppend('section', menuSection);
 menuItem1.classList.add('item');
 createAndAppend('h3', menuItem1, 'Café Espresso');
 createAndAppend('p', menuItem1, 'El clásico espresso italiano, fuerte y aromático.');
-createAndAppend('span', menuItem1, '$2.99');
+createAndAppend('span', menuItem1, '$8.000');
+createAndAppend('img', menuItem1).setAttribute('src', 'espresso.png'); // Reemplaza 'espresso.jpg' con la ruta de tu imagen
+
+const menuItem2 = createAndAppend('section', menuSection);
+menuItem2.classList.add('item');
+createAndAppend('h3', menuItem2, 'Café Latte');
+createAndAppend('p', menuItem2, 'Café suave con leche caliente y espuma.');
+createAndAppend('span', menuItem2, '$12.000');
+createAndAppend('img', menuItem2).setAttribute('src', 'latte.png'); // Reemplaza 'latte.jpg' con la ruta de tu imagen
+
+// ... (agrega más elementos al menú si es necesario)
 
 // Crear sección de promociones (ejemplo)
 const promotionsSection = createAndAppend('section', main);
+promotionsSection.classList.add('promotions'); // Agregamos la clase 'promotions'
+promotionsSection.setAttribute('id', 'promociones'); // Agregamos el atributo id
 createAndAppend('h2', promotionsSection, 'Promociones');
 
-const promotionItem1 = createAndAppend('div', promotionsSection);
+const promotionItem1 = createAndAppend('section', promotionsSection);
 promotionItem1.classList.add('promotion');
 createAndAppend('h3', promotionItem1, 'Promoción del Día');
 createAndAppend('p', promotionItem1, '¡Hoy, compra un café grande y obtén una galleta gratis!');
