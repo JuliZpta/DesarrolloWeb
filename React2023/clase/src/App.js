ee015f519c6741edce3129beb115c3ae6021c5a8
JuliZpta <zapatajulian42@gmail.com>
2023-10-17 11:29:30 -0500

    Routes

--- React2023/clase/src/App.js
+++ React2023/clase/src/App.js
@@ -1,9 +1,14 @@
 import Header from './componentes/Header';
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element ={<Header />}></Route>
+      </Routes>
+      </BrowserRouter>
     
     </div>
   );
